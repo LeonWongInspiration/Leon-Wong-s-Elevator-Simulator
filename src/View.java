7f795b75a9a8882298cb410311eecce5943cd83f
LeonWongInspiration <leonwong.inspiration@gmail.com>
2018-04-26 05:48:30 +0800

    Update Layout

--- src/View.java
+++ src/View.java
@@ -1,7 +1,15 @@
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class View extends Application {
+
+    private Stage primaryStage;
+    private BorderPane layout;
 
     public static void main(String[] args) {
         launch(args);
@@ -9,6 +17,26 @@ public class View extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Elevator Simulator");
 
+        this.initLayout();
+    }
+
+    public void initLayout(){
+        try {
+            // Load layout from FXML file
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Application.class.getResource("View.fxml"));
+            this.layout = (BorderPane) loader.load();
+
+            // Show the scene
+            Scene scene = new Scene(this.layout);
+            this.primaryStage.setScene(scene);
+            this.primaryStage.show();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
